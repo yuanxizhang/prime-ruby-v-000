bd5ae9d9219e81202ee479dc35f694f8dd424f6c
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-05 22:27:02 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # check if an integer is prime, return true or false
 def prime?(n)
 
-  Math.sqrt(num).floor.downto(2).each {|i| return false if num % i == 0}
+  Math.sqrt(n).floor.downto(2).each {|i| return false if n % i == 0}
   true
   
 end
