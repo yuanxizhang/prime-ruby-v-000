bfeca46f4cfbc237afcf4e7b829b351776a429ca
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-05 22:02:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+# return true or FALSE
+def prime?(n)
+  if n.even?
+    false 
+  else
+    3..n.each do |x| 
+      if n % x == 0 && x != n
+        return false
+      else
+        return true
+   end
+end
