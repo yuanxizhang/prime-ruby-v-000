c35dbd8084229a621ebb63f1e406f3cba9e679af
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-05 22:11:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # check if an integer is prime, return true or false
 def prime?(n)
 
-    3..n.each do |x| 
+    2..n.each do |x| 
         if n % x == 0 && x != n
           return false
         else
